5fe249b4cf91006fe3a20a0540e191cfdd3b7a3a
thgr8ganzi <q33asq33as@gmail.com>
2022-01-16 12:39:49 +0900

    소수찾기

---
+++ src/codingTest/FindingThePrimeNumber.java
@@ -0,0 +1,26 @@
+package codingTest;
+
+public class FindingThePrimeNumber {
+
+    public int solution(int n) {
+        int answer = 0;
+        int arr[] = new int[n+1];
+        arr[0] = 0; arr[1] = 0;
+
+        for (int i = 2 ; i <= n; i++) { arr[i] = i; }
+
+        for (int i = 2 ; i <= (int)Math.sqrt(n) ; i++) {
+            if(arr[i] == 0) continue;
+            for (int j = 2 * i ; j <= n; j+=i) {
+                arr[j] = 0;
+            }
+        }
+
+        for (int i = 0; i <= n ; i++ ) {
+            if (arr[i] != 0) answer++;
+        }
+
+        return answer;
+    }
+}
+//  https://programmers.co.kr/learn/courses/30/lessons/12921
